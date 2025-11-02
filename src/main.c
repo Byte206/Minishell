ed5d5162c256f768aef91a3e783de16b10e8e1ac
Bastian Monterde Pedra <bmonterd@c2r7s1.42madrid.com>
2025-11-02 14:58:24 +0100

    Added  Quote_struct, restructured main

--- src/main.c
+++ src/main.c
@@ -6,28 +6,47 @@
 /*   By: bmonterd <bmonterd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 14:30:23 by bmonterd          #+#    #+#             */
-/*   Updated: 2025/11/01 15:38:22 by bmonterd         ###   ########.fr       */
+/*   Updated: 2025/11/02 14:53:21 by bmonterd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../includes/minishell.h"
 
-
-int	main(void)
+int main(int argc, char **argv, char **envp)
 {
-	char	*input;
-	t_token	*tokens;
-	t_ast	*ast;
-
-	while (1)
-	{
-		input = readline("minishell> ");
-		if (!input)
-			break;
-		if (*input)
-			add_history(input);
-	}
-	printf("exit\n");
-	return (0);
+    // t_env   *env; Gab
+    char    *input;
+    t_token *tokens;
+    t_ast   *ast;
+    
+    (void)argc;
+    (void)argv;
+    // g_exit_code = 0; Gab used on signals
+    // env = init_env(envp); Gab
+    // setup_signals(); Gab signals
+    while (1)
+    {
+        input = readline("minishell> ");
+        if (!input)
+            break;
+        if (*input)
+        {
+            add_history(input);
+            // tokens = tokenize(input); Bast
+            // if (tokens)
+            // {
+            //     ast = parse(tokens); Bast
+            //     if (ast)
+            //     {
+            //         g_exit_code = execute(ast, env); Gab
+            //         free_ast(ast);
+            //     }
+            //     free_tokens(tokens); Bast
+            // }
+        }
+        free(input);
+    }
+    printf("exit\n");
+    // free_env(env); Gab
+    return (1/*g_exit_code*/);  //Gab signals
 }
